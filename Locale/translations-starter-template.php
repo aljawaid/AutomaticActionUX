36d24fa3915299b372cd0cb5597c5b16c5314b57
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-17 21:44:16 +0000

    Update Translation Starter Template
    - #12 Translate Plugin

--- Locale/translations-starter-template.php
+++ Locale/translations-starter-template.php
@@ -1,16 +1,76 @@
-<!-- REMOVE THIS SECTION -->
-<!-- ------------------- -->
-<!-- TEMPLATE FILE FOR LOCAL TRANSLATIONS - KEEP FILENAME IN LOWERCASE AS translations.php UNDER LANGUAGE CODE -->
-<!-- EXAMPLE FILE: /Locale/en_GB/translations.php -->
-<!-- EXAMPLE FILE: /Locale/en_US/translations.php -->
-<!-- EXAMPLE FILE: /Locale/fr_FR/translations.php -->
-<!-- EXAMPLE FILE: /Locale/de_DE/translations.php -->
-<!-- ------------------- -->
-<!-- REMOVE THIS SECTION -->
 <?php
 return array(
   //
   // GENERAL
   //
-  '' => '',
+  'This plugin gives the Automatic Action interface a complete makeover to make it more user friendly. Particularly useful for visual learners and non-English speaking users, this plugin adds a quick glance of Actions on the board avoiding careless drag-happy mistakes.' => '',
+  //
+  // CORE OVERRIDES OR 3RD PARTY PLUGIN TRANSLATIONS AFFECTING THIS PLUGIN
+  //
+  'Automatic actions' => '',
+  'Duration in days' => '',
+  'Assignee' => '',
+  'Destination swimlane' => '',
+  //
+  // project_header/actions.php
+  //
+  'Automatic Actions' => '',
+  'Actions for this project' => '',
+  'View all actions for this project' => '',
+  'User Triggered Actions' => '',
+  'System Triggered Actions' => '',
+  //
+  // action/index.php
+  //
+  'Automatic Actions for Project' => '',
+  'Add a new action' => '',
+  'Import from another project' => '',
+  'There is no action at the moment.' => '',
+  'Line Number' => '',
+  'Trigger' => '',
+  'Relation' => '',
+  'Event Activity' => '',
+  'Action Title' => '',
+  'Action Details' => '',
+  'This ID is for the Action not the Action parameter' => '',
+  'Action ID' => '',
+  'System' => '',
+  'User' => '',
+  'Task' => '',
+  'Subtask' => '',
+  'Move Column' => '',
+  'Task Create' => '',
+  'Task Create/Edit' => '',
+  'Task Update' => '',
+  'Move Swimlane' => '',
+  'Task Close' => '',
+  'Task Assignee Change' => '',
+  'Assignee Change' => '',
+  'GitLab' => '',
+  'GitHub' => '',
+  'No Activity' => '',
+  'No Activity Column' => '',
+  'No Activity Task' => '',
+  'Subtasks' => '',
+  'Comment Creation' => '',
+  'Assign Color' => '',
+  'Assign User' => '',
+  'Close Task' => '',
+  'Create Task' => '',
+  'Send Email' => '',
+  'Move to Swimlane' => '',
+  'Move Swimlane' => '',
+  'Update Start Date' => '',
+  'Start Date' => '',
+  'Create Subtask' => '',
+  'Email Group' => '',
+  'Email Task History' => '',
+  'Email History' => '',
+  'Email Assignee' => '',
+  'Subtasks Due' => '',
+  'Create Subtasks and Activate Timers' => '',
+  'Activate Timers' => '',
+  'Delete Automatic Action' => '',
+  'Automatic Action not found: "%s"' => '',
+  'Event Name' => '',
 );
